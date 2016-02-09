9ffce58297370529238d58feec5ef92e2ea63398
eaden <kmk@gmx.de>
2016-02-09 18:32:08 +0100

    cin cout + dynamische typisierung

--- HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld.py
@@ -1,4 +1,11 @@
-﻿# My first program
-print("""This is a comment
-over multiple lines which also
-is written over multiples output lines without escapes """)
+﻿# cin cout + dynamic type
+print("Please type in sth")
+name = input()
+print(name)
+name2 = "fellow"
+name3 = name+name2
+print(name3)
+name3 = 7/3
+print(name3)
+p = 2+2.0
+print(p)
