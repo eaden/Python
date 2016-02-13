d4891afbc5fc582d98b99174847dcb292ec2dafa
eaden <kmk@gmx.de>
2016-02-13 03:02:13 +0100

    # cin cout + string cast, string methods + math

--- HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld.py
@@ -1,11 +1,12 @@
-﻿# cin cout + dynamic type
-print("Please type in sth")
-name = input()
-print(name)
-name2 = "fellow"
-name3 = name+name2
-print(name3)
-name3 = 7/3
-print(name3)
-p = 2+2.0
-print(p)
+﻿# cin cout + string cast, string methods + math
+print("Please type in your last name")
+p = input()
+p = p.lower()
+p = p.capitalize()
+print("Your last name is " + p)
+print("Please type in a number that will be calculated by the power of 3")
+p = int(input())
+p = p**3
+string = "The calculated number is {}".format(p)
+print(string)
+input()
